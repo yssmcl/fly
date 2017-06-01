860549d4ce852fc41b42e168c3094d7c0fe7bc96
Felipe Nardi <felipebn@hotmail.com.br>
2017-06-01 16:46:08 -0300

    Added css example.

--- Code/fly/curso_extensao/views.py
+++ Code/fly/curso_extensao/views.py
@@ -11,4 +11,4 @@ class IndexView(View):
 
 class NovoCursoExtensao(generic.CreateView):
     model = CursoExtensao
-    fields = ['titulo','coordenador','periodo_de_realizacao','programa_extensao','unidade_administrativa','campus','centro','grande_area','area_tematica_principal','area_tematica_secundaria','linha_extensao','publico_alvo','numero_pessoas_beneficiadas','carga_horaria_total','numero_vagas','local_inscricao','resumo','programacao','servidores','previsao_orcamentaria']
+    fields = ['titulo','coordenador','periodo_de_realizacao','programa_extensao','unidade_administrativa','campus','centro','grande_area','area_tematica_principal','area_tematica_secundaria','linha_extensao','publico_alvo','numero_pessoas_beneficiadas','carga_horaria_total','numero_vagas','local_inscricao','resumo','programacao','previsao_orcamentaria']
