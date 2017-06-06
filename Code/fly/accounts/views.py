72c5ab7c7f26e054be277bf830d28895bff88129
felipebnn <felipebn@hotmail.com.br>
2017-06-05 22:06:33 -0300

    Added style to the templates.

--- Code/fly/accounts/views.py
+++ Code/fly/accounts/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.db.models import F
 from django.contrib.auth import views as auth_views
-from django.views import View, generic
 
 class LoginView(auth_views.LoginView):
 	template_name = 'accounts/login.html'
