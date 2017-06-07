4d99cecf23f04a839a3b29c2b3832d4af4783ec5
LEANDRO\Leandro Ensina <leandro.ensina95@gmail.com>
2017-06-07 11:12:46 -0300

    finalizacao da tela de cadastro de curso de extensao

--- Code/fly/curso_extensao/forms.py
+++ Code/fly/curso_extensao/forms.py
@@ -11,7 +11,7 @@ class CursoExtensaoForm(forms.ModelForm):
         fields = ['titulo', 'coordenador', 'periodo_de_realizacao', 'programa_extensao', 'unidade_administrativa', 'campus', 'centro', 'grande_area', 'area_tematica_principal', 'area_tematica_secundaria', 'linha_extensao', 'publico_alvo', 'numero_pessoas_beneficiadas', 'carga_horaria_total', 'numero_vagas', 'local_inscricao', 'resumo', 'programacao', 'previsao_orcamentaria']
 
 
-Servidor_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, Servidor_CursoExtensao, extra=2, fields=['servidor', 'carga_horaria_dedicada', 'funcao'])
+Servidor_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, Servidor_CursoExtensao, extra=1, fields=['servidor', 'carga_horaria_dedicada', 'funcao'])
 PalavraChave_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, PalavraChave_CursoExtensao, extra=3, min_num=1, max_num=3, fields=['nome'])
-Discente_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, Discente_CursoExtensao, extra=2, fields=['nome', 'curso', 'serie', 'turno', 'carga_horaria_semanal', 'telefone', 'email'])
-MembroComunidade_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, MembroComunidade_CursoExtensao, extra=2, fields=['nome', 'carga_horaria_semanal', 'entidade', 'telefone', 'email', 'cpf', 'data_nascimento', 'funcao'])
+Discente_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, Discente_CursoExtensao, extra=1, fields=['nome', 'curso', 'serie', 'turno', 'carga_horaria_semanal', 'telefone', 'email'])
+MembroComunidade_CursoExtensaoFormSet = inlineformset_factory(CursoExtensao, MembroComunidade_CursoExtensao, extra=1, fields=['nome', 'carga_horaria_semanal', 'entidade', 'telefone', 'email', 'cpf', 'data_nascimento', 'funcao'])
