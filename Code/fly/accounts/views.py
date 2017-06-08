1a4c76585232e61058dfb829c213d57dc841d24b
Felipe Nardi <felipebn@hotmail.com.br>
2017-06-08 09:34:26 -0300

    Adicionado botao de logar e deslogar.

--- Code/fly/accounts/views.py
+++ Code/fly/accounts/views.py
@@ -1,4 +1,7 @@
 from django.contrib.auth import views as auth_views
 
 class LoginView(auth_views.LoginView):
-	template_name = 'accounts/login.html'
+	pass
+
+class LogoutView(auth_views.LogoutView):
+	pass
