f2e18550640b9c8055d3c1c767ba0e00daaa853e
LEANDRO\Leandro Ensina <leandro.ensina95@gmail.com>
2017-07-03 23:22:14 -0300

    Alteracao do models.py do relatorio

--- Code/fly/relatorio/models.py
+++ Code/fly/relatorio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from curso_extensao.models import CursoExtensao
+
+class Relatorio(models.Model):
+    projeto_extensao = models.ForeignKey(CursoExtensao)
+
+    periodo_realizacao_inicio = models.DateField()
+    periodo_realizacao_fim = models.DateField()
+
+    colegiado = models.CharField(max_length=200)
+
+    publico_atingido = models.CharField(max_length=200)
