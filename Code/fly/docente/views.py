d14c6b4322e82938711bebd7ceb0d2aee7c30fd9
yssmcl <yuri_1704@hotmail.com>
2017-07-07 01:40:48 -0300

    Implementa consulta e alteração de Docente

--- Code/fly/docente/views.py
+++ Code/fly/docente/views.py
@@ -31,14 +31,34 @@ class ConsultaDocente(LoginRequiredMixin, generic.ListView):
     model = Docente
 
     def get_queryset(self):
-        d = { }
-
+        d = {}
         if 'nome_completo' in self.request.GET:
             d['nome_completo__contains'] = self.request.GET.get('nome_completo', '')
+            d['email__contains'] = self.request.GET.get('email', '')
 
         return Docente.objects.filter(**d)
 
 
 class DetalheDocente(LoginRequiredMixin, View):
     def get(self, request, pk):
-        pass
+        docente = get_object_or_404(Docente, pk=pk)
+
+        main_form = DocenteForm(instance=docente, prefix='main')
+
+        return render(request, 'docente/docente_form.html', {'main_form': main_form})
+
+    def post(self, request, pk):
+        docente = get_object_or_404(Docente, pk=pk)
+
+        main_form = DocenteForm(request.POST, instance=docente, prefix='main')
+
+        docente = main_form.instance
+
+        if main_form.is_valid():
+            with transaction.atomic():
+                main_form.save()
+
+            return redirect('curso_extensao:index')
+
+        else:
+            return render(request, 'docente/docente_form.html', {'main_form': main_form})
